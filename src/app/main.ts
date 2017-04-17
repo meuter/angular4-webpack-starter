ab4b17490aff871610a351cfe9715952b7c2c47b
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-17 16:37:13 +0200

    enableProdMode

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './modules/app.module';
+import {enableProdMode} from '@angular/core';
 
-// TODO(cme): enableProdMode is env is set
-
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
