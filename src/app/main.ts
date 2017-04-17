6faafbbbd9085a6c1bc7c9ff99cc542d1e2c05a4
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-17 16:07:10 +0200

    Uniformize webpack output

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './modules/app.module';
 
+// TODO(cme): enableProdMode is env is set
+
 platformBrowserDynamic().bootstrapModule(AppModule);
