68ec08e1ee94c05c56afb58d4627077717817b3b
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-18 19:36:10 +0200

    Added basic example to demonstrate the use of ngx-pipe

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -7,6 +7,11 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
     readonly greetings = "Hello World";
-    readonly foos = [ "foo", "bar", "baz" ];
+    readonly persons = [
+        { "name" : "john", "age" : 37 },
+        { "name" : "joe", "age" : 25 },
+        { "name" : "alan", "age": 52 }
+    ];
+
     @Input() inputValue: string;
 }
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,13 +2,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgPipesModule } from 'ngx-pipes';
 
 // Components
 import { AppComponent } from '../components/app.component';
 
 @NgModule({
   declarations: [ AppComponent ],
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, NgPipesModule ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
