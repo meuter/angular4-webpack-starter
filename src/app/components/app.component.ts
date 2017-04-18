71cb6e993c53930b38a4fa944935c27fb591098c
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-18 19:07:36 +0200

    Added a list to check that *ngFor works

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
     styleUrls: [ "./app.component.css" ]
 })
 export class AppComponent {
-    readonly greetings : string = "Hello World";
+    readonly greetings = "Hello World";
+    readonly foos = [ "foo", "bar", "baz" ];
 }
